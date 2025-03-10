4d6e3e3e1a52548293b83a13ba5db98677888108
WilmerProfesor <wilmerprofesor@gmail.com>
2025-03-10 11:38:30 -0500

    File class JS

---
+++ ClaseJavaScript_1/js/main.js
@@ -0,0 +1,69 @@
+// const PI= 4.1416;
+
+// const persona={nombre: "Juan", apellido: "paramo", edad: 18};
+// const arreglo=[1,234,"ddfsd",[23,343],{nombre:"juan", apellido:"algo"}];
+
+// let nombre= prompt("Ingrese su nombre"); //jOptionPane.showInputDialog()
+// let edad= prompt("Ingrese su edad"); //jOptionPane.showInputDialog()
+// alert("Edad  "+Number(edad)); // jOptionPane.showMessageDialog()
+
+// console.log("Hola "+nombre);  // SOUT
+//'_' "_" `_` 
+// console.log(arreglo);
+
+// let nombre="wilmer";
+// console.log(`hola ${nombre}`);
+// console.log('"hola mundo"');
+// console.log("'hola mundo'");
+// let varA=3;
+// let varB="3";
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);    
+// }
+
+
+// if(varA===varB){
+//     console.log("Son iguales");
+// }else{
+//     console.log("Diferentes");
+    
+// }
+// console.log(typeof(varA));
+// console.log(typeof(varB));
+// console.log(varB);
+
+
+// for (let index = 0; index < 10; index++) {    
+//     if(index===5){
+//         continue
+//     }
+//     console.log(index);    
+// }
+
+// const object ={nombre: "Wilmer", apellido: "patiño", edad:37};
+// for (const key in object) {
+//     console.log(object[key]);
+// }
+
+// const array =["Patiño", "Wilmer", 37];
+// for (const index of array) {
+//     console.log(index);
+// }
+// let pass=123;
+// let dato=1;
+// while(dato!=pass){
+//     dato=prompt("Ingrese la contraseña")
+// }
+// let password=123;
+// let ingresado=1;
+
+// do{
+//     ingresado=prompt("Ingrese la contraseña con do-while")
+// }while(ingresado!=password)
+
+// let elemento=document.g.getElementById("texto");
+// elemento.innerHTML+="Hola mundo desde JS"
+
+const array =["Patiño", "Wilmer", 37];
+console.log(array.reverse());
+console.log(array);
